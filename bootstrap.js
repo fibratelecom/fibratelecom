01a2bc129739ad0d71cf5670ae929226a167e10d
fibratelecom <lanconta2015@gmail.com>
2026-08-30 19:22:50 -0300

    Atualiza carregamento apos isolar Chamados

--- bootstrap.js
+++ bootstrap.js
@@ -1,6 +1,6 @@
 (async()=>{
   window.__PROVEDOR_PLUS_CLOUD__=true;
-  const BUILD_TOKEN='20260830-1928-chamados-isolado1';
+  const BUILD_TOKEN='20260830-1945-chamados-fixo1';
   window.__PROVEDOR_PLUS_BUILD__=BUILD_TOKEN;
   const assetUrl=value=>{
     const src=String(value||'');

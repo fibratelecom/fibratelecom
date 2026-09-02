c86cbe4e2600fe678bb1a32b6ff41c6eaf9e38d5
fibratelecom <lanconta2015@gmail.com>
2026-09-02 13:35:48 -0300

    Liga reacoes dos Stories ao Worker

--- stories-entry-worker.js
+++ stories-entry-worker.js
@@ -1,9 +1,11 @@
 import baseWorker from './negotiation-worker.js';
 import {handleStoriesRequest,isStoriesPath} from './stories-worker.js';
+import {handleStoryReactionsRequest,isStoryReactionsPath} from './story-reactions-worker.js';
 
 export default {
   fetch(request,env,ctx){
     const path=new URL(request.url).pathname;
+    if(isStoryReactionsPath(path))return handleStoryReactionsRequest(request,env);
     if(isStoriesPath(path))return handleStoriesRequest(request,env);
     return baseWorker.fetch(request,env,ctx);
   },

a71ddb345b845465b4d03b31c15a7abed8416024
fibratelecom <lanconta2015@gmail.com>
2026-09-02 10:27:24 -0300

    Liga stories ao Worker principal

---
+++ stories-entry-worker.js
@@ -0,0 +1,11 @@
+import baseWorker from './negotiation-worker.js';
+import {handleStoriesRequest,isStoriesPath} from './stories-worker.js';
+
+export default {
+  fetch(request,env,ctx){
+    const path=new URL(request.url).pathname;
+    if(isStoriesPath(path))return handleStoriesRequest(request,env);
+    return baseWorker.fetch(request,env,ctx);
+  },
+  scheduled(controller,env,ctx){return baseWorker.scheduled(controller,env,ctx)}
+};
